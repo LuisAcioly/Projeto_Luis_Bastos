650fae9962b646584f110b4cdc4075dd17b49dd7
Luis_Bastos <luis.bastos@estudante.com.br>
2020-11-17 19:09:54 -0300

    Primeiro commit

---
+++ classeA.java
@@ -0,0 +1,36 @@
+public class classeA {
+    private int A1;
+    private float A2;
+
+    public classeA(int A1, float A2){
+        this.A1 = A1;
+        this.A2 = A2;
+    }
+
+    public int getA1(){
+        return this.A1;
+    }
+
+    public float getA2(){
+        return this.A2;
+    }
+
+    public void setA1(int A1){
+        this.A1 = A1
+    }
+
+    public void setA2(int A2){
+        this.A2 = A2
+    }
+
+    public void MA1() {
+        System.out.print("MA1");
+
+    }
+
+    public void MA2() {
+        System.out.print("MA2");
+
+    }
+
+}
---
+++ classeB.java
@@ -0,0 +1,40 @@
+public class classeB {
+    private int B1;
+    private float B2;
+
+    public classeA(int B1, float B2){
+        this.B1 = B1;
+        this.B2 = B2;
+    }
+
+    public int getB1(){
+        return this.B1;
+    }
+
+    public float getB2(){
+        return this.B2;
+    }
+
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setA2(int B2){
+        this.B2 = B2;
+    }
+
+    public void MB1() {
+        System.out.print("MB1");
+
+    }
+
+    public void MB2() {
+        System.out.print("MB2");
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
